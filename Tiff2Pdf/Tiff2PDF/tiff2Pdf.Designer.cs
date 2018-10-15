37655a259d1f6986d5d9c17685eb29772dcd6954
Scott Donaldson <43659841+Scott-Donaldson@users.noreply.github.com>
2018-10-15 19:41:37 +0100

    Add files via upload

---
+++ Tiff2Pdf/Tiff2PDF/tiff2Pdf.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Tiff2PDF
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbd_mainFBD = new System.Windows.Forms.FolderBrowserDialog();
+            this.Rtb_output = new System.Windows.Forms.RichTextBox();
+            this.btn_tiffDir = new System.Windows.Forms.Button();
+            this.lbl_tiffDir = new System.Windows.Forms.Label();
+            this.txt_tiffDir = new System.Windows.Forms.TextBox();
+            this.lbx_tiffFiles = new System.Windows.Forms.ListBox();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Rtb_output
+            // 
+            this.Rtb_output.Location = new System.Drawing.Point(12, 300);
+            this.Rtb_output.Name = "Rtb_output";
+            this.Rtb_output.ReadOnly = true;
+            this.Rtb_output.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.Rtb_output.Size = new System.Drawing.Size(244, 138);
+            this.Rtb_output.TabIndex = 0;
+            this.Rtb_output.Text = "";
+            // 
+            // btn_tiffDir
+            // 
+            this.btn_tiffDir.Location = new System.Drawing.Point(16, 36);
+            this.btn_tiffDir.Name = "btn_tiffDir";
+            this.btn_tiffDir.Size = new System.Drawing.Size(121, 23);
+            this.btn_tiffDir.TabIndex = 1;
+            this.btn_tiffDir.Text = "Select Tiff Directory";
+            this.btn_tiffDir.UseVisualStyleBackColor = true;
+            this.btn_tiffDir.Click += new System.EventHandler(this.btn_tiffDir_Click);
+            // 
+            // lbl_tiffDir
+            // 
+            this.lbl_tiffDir.AutoSize = true;
+            this.lbl_tiffDir.Location = new System.Drawing.Point(13, 13);
+            this.lbl_tiffDir.Name = "lbl_tiffDir";
+            this.lbl_tiffDir.Size = new System.Drawing.Size(70, 13);
+            this.lbl_tiffDir.TabIndex = 2;
+            this.lbl_tiffDir.Text = "Tiff Directory:";
+            // 
+            // txt_tiffDir
+            // 
+            this.txt_tiffDir.Enabled = false;
+            this.txt_tiffDir.Location = new System.Drawing.Point(90, 10);
+            this.txt_tiffDir.Name = "txt_tiffDir";
+            this.txt_tiffDir.Size = new System.Drawing.Size(166, 20);
+            this.txt_tiffDir.TabIndex = 3;
+            this.txt_tiffDir.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbx_tiffFiles
+            // 
+            this.lbx_tiffFiles.Enabled = false;
+            this.lbx_tiffFiles.FormattingEnabled = true;
+            this.lbx_tiffFiles.Location = new System.Drawing.Point(12, 69);
+            this.lbx_tiffFiles.Name = "lbx_tiffFiles";
+            this.lbx_tiffFiles.Size = new System.Drawing.Size(244, 225);
+            this.lbx_tiffFiles.TabIndex = 4;
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(143, 36);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(113, 23);
+            this.btn_convert.TabIndex = 5;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 450);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.lbx_tiffFiles);
+            this.Controls.Add(this.txt_tiffDir);
+            this.Controls.Add(this.lbl_tiffDir);
+            this.Controls.Add(this.btn_tiffDir);
+            this.Controls.Add(this.Rtb_output);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.ShowIcon = false;
+            this.Text = "Tiff to PDF Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbd_mainFBD;
+        private System.Windows.Forms.RichTextBox Rtb_output;
+        private System.Windows.Forms.Button btn_tiffDir;
+        private System.Windows.Forms.Label lbl_tiffDir;
+        private System.Windows.Forms.TextBox txt_tiffDir;
+        private System.Windows.Forms.ListBox lbx_tiffFiles;
+        private System.Windows.Forms.Button btn_convert;
+    }
+}
+
---
+++ Tiff2Pdf/Tiff2PDF/tiff2Pdf.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Tiff2PDF
+{
+    public partial class mainForm : Form
+    {
+
+        public String tiffDir = "";
+        string[] tiffFiles;
+
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void chooseTiffDir()
+        {
+            FolderBrowserDialog fbd_mainFBD = new FolderBrowserDialog();
+            if (fbd_mainFBD.ShowDialog() == DialogResult.OK)
+            {
+                tiffDir = fbd_mainFBD.SelectedPath;
+
+                tiffFiles = Directory.GetFiles(@tiffDir, "*.tif");
+                if (tiffFiles.Length == 0)
+                {
+                    Rtb_output.AppendText("\nNo Tiff Files in" + tiffDir);
+                    DialogResult dr = MessageBox.Show("Would you like to select another directory?", "No Tiff Files Found!", MessageBoxButtons.YesNo);
+                    switch (dr)
+                    {
+                        case DialogResult.Yes:
+                            chooseTiffDir();
+                            break;
+                        case DialogResult.No:
+                            return;
+                    }
+                }
+                else
+                {
+                    Rtb_output.AppendText("\n" + tiffFiles.Length + " Tiff File(s) Found");
+                    txt_tiffDir.Text = fbd_mainFBD.SelectedPath;
+                    for(int i = 0; i < tiffFiles.Length; i++)
+                    {
+                        lbx_tiffFiles.Items.Add(tiffFiles[i].Substring(txt_tiffDir.Text.Length + 1));
+                        Rtb_output.AppendText("\n"+ tiffFiles[i].Substring(txt_tiffDir.Text.Length + 1)+" found!");
+                    }
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Rtb_output.AppendText("Launch: Successful");
+            
+        }
+
+        private void btn_tiffDir_Click(object sender, EventArgs e)
+        {
+            chooseTiffDir();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_PdfDir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < tiffFiles.Length; i++)
+            {
+                try
+                {
+                    ConvertTiffToPdf(tiffDir+"/"+lbx_tiffFiles.Items[i].ToString(), lbx_tiffFiles.Items[i].ToString());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private string ConvertTiffToPdf(string tiffFileName, string fileName)
+        {
+            //iTextSharp.text.Rectangle pgSize = PageSize.LETTER;
+            iTextSharp.text.Rectangle pgSize = PageSize.LEGAL;
+            string pdfFile;
+            string[] FileName = fileName.Split('.');
+            do
+            {
+                pdfFile = Path.GetDirectoryName(tiffFileName) + Path.DirectorySeparatorChar +
+                    FileName[0] + ".pdf";
+            } while (File.Exists(pdfFile));
+            if (File.Exists(pdfFile))
+            {
+                File.Delete(pdfFile);
+            }
+            if (!File.Exists(tiffFileName))
+            {
+                return null;
+            }
+
+            Bitmap bmp = new Bitmap(tiffFileName);
+
+            int totalPages = bmp.GetFrameCount(FrameDimension.Page);
+
+            float margin = 0.0f;
+            //float margin = 50.0f;
+            Document document = new Document(pgSize, margin, margin, margin, margin);
+
+            try
+            {
+
+                // creation of the different writers
+                PdfWriter writer = PdfWriter.GetInstance(document,
+                    new FileStream(pdfFile, FileMode.Create));
+
+                // Which of the multiple images in the TIFF file do we want to load
+                // 0 refers to the first, 1 to the second and so on.
+                document.Open();
+                PdfContentByte cb = writer.DirectContent;
+                iTextSharp.text.Image img;
+
+                for (int ii = 0; ii < totalPages; ii++)
+                {
+                    bmp.SelectActiveFrame(FrameDimension.Page, ii);
+                    img = iTextSharp.text.Image.GetInstance(BitmapToBytes(bmp));
+                    img.ScalePercent(72f / bmp.HorizontalResolution * 100);
+                    img.SetAbsolutePosition(0, 0);
+                    cb.AddImage(img);
+                    document.NewPage();
+                }
+
+                document.Close();
+                document = null;
+                bmp.Dispose();
+                bmp = null;
+                Rtb_output.AppendText("\n"+FileName+" Converted");
+
+            }
+            catch (Exception ex)
+            {
+                this.Cursor = Cursors.Default;
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+
+            return pdfFile;
+        }
+        private byte[] BitmapToBytes(Bitmap bmp)
+        {
+            byte[] data = new byte[0];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bmp.Save(ms, ImageFormat.Png);
+                ms.Seek(0, 0);
+                data = ms.ToArray();
+            }
+            return data;
+        }
+
+
+    }
+}
